2990eac14c36b6e1559768a9d656c0150d1f9111
Levon <milevon@gmail.com>
2022-12-25 16:46:57 +0400

    header is ready

--- irecruiter/src/App.js
+++ irecruiter/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { useEffect } from 'react';
+import { db } from './db/firebase'
+import { query, collection } from 'firebase/firestore'
+import Header from './components/header/Header';
+
 
 function App() {
+
+  useEffect(() => { 
+    const q = query(collection(db, 'users'))
+    console.log(q)
+  },[])
   return (
     <div className="App">
-
+      <Header/>
     </div>
   );
 }
---
+++ irecruiter/src/components/header/Header.jsx
@@ -0,0 +1,45 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import HelpIcon from "@mui/icons-material/Help";
+import { Link } from "react-router-dom";
+import "./HeaderStyles.css";
+
+const Header = () => {
+  return (
+    <Box>
+      <AppBar position="sticky">
+        <Box className="navbar">
+          <Toolbar className="toolbar">
+            <div>
+              <IconButton>
+                <MenuIcon fontSize="large" sx={{ color: "white" }} />
+                          </IconButton>
+                          <span><Link to="/"className="home_link">Home</Link></span>
+                      </div>
+                   
+            <input
+              className="search_input"
+              type="text"
+              placeholder="Search by Name, Job, Email or Client"
+            ></input>
+            <div>
+              <IconButton>
+                <AddCircleIcon sx={{ color: "white" }} />
+              </IconButton>
+              <IconButton>
+                <HelpIcon sx={{ color: "white" }} />
+              </IconButton>
+              <div className="container">
+                <div className="user_avatar">L</div>
+              </div>
+            </div>
+          </Toolbar>
+        </Box>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
