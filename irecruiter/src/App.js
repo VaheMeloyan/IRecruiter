869ca7f5ab38160ab39b66f8a7f376a398f16ab3
Levon <milevon@gmail.com>
2022-12-28 22:28:49 +0400

    signup merged

--- irecruiter/src/App.js
+++ irecruiter/src/App.js
@@ -4,15 +4,14 @@ import { db } from './db/firebase'
 import {collection, getDocs} from 'firebase/firestore'
 import Header from './components/header/Header';
 import { useState } from 'react';
-import LoginPage from './components/login/LoginPage';
 import { Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp'
 
 
 function App() {
   return (
-    <div className="App">
-      <SignUp/>
+    <div>
+   <SignUp/>
     </div>
   );
 }
