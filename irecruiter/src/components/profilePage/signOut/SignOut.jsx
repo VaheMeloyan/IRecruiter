c3f89d8bb0f9c082a28e4f93c0948e601faf2123
Levon <milevon@gmail.com>
2023-01-01 16:43:19 +0400

    responsivness for login and signup added

---
+++ irecruiter/src/components/profilePage/signOut/SignOut.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SignOutStyles.css'
+
+const SignOut = () => {
+  return (
+      <div className='signout-dropdown-container'>
+          <div><span>Name:</span><span>Levon</span></div>
+    </div>
+  )
+}
+
+export default SignOut
